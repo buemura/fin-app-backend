55b7a9777f288dd40d4f43fdb247fe225241a717
BrunoUemura <bruno.h.uemura@outlook.com>
2023-02-07 22:04:59 -0300

    fix: changes route definition order

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,14 +6,13 @@ import { accountRouter } from "./account-routes";
 
 const router = Router();
 
-router.use("/api", userRouter, expenseRouter, accountRouter);
-
 router.get("/", (_request: Request, response: Response) => {
   return response.send({
     message: "Access api through /api endpoint",
   });
 });
 
+router.use("/api", userRouter, expenseRouter, accountRouter);
 router.get("/api/health", (_request: Request, response: Response) => {
   return response.send({
     message: "API is up and running",
