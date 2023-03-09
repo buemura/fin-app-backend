9cf1e91f0a5fa69b0eba73ebb933e1c062475f61
BrunoUemura <bruno.h.uemura@outlook.com>
2023-03-09 19:36:59 -0300

    fix: fixes tests 2

--- src/infra/http/configs/tests/server-config.spec.ts
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import { server } from "../server-config";
-
-describe.skip("Server Config test", () => {
-  it("should export server config", () => {
-    expect(server).toBeInstanceOf(http.Server);
-  });
-});
