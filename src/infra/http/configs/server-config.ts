8512be1a7c5eefe37ff8bc36135b6df649210364
BrunoUemura <bruno.h.uemura@outlook.com>
2023-03-09 20:00:17 -0300

    fix: adds cors origin

--- src/infra/http/configs/server-config.ts
+++ src/infra/http/configs/server-config.ts
@@ -7,7 +7,11 @@ import { router } from "../routes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5173", "https://app-fin.vercel.app"],
+  })
+);
 app.use(express.json());
 app.use(router);
 
